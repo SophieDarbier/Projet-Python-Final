7966aaac99b532f9e0bd4b30a4c466a105c11a82
uwu1111 <118531895+uwu1111@users.noreply.github.com>
2023-10-12 16:43:46 +0200

    api changes

--- server/api.py
+++ server/api.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+from . import models
+from .database import SessionLocal, engine
 
 app = FastAPI()
 
+# Dependency
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
